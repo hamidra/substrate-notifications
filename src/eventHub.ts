51cfc91ceef276d28c94c78ffe53436a7eb7f06d
hamidra <hamid.alipour@gmail.com>
2021-07-09 15:32:54 -0700

    add eventhub

--- src/eventHub.ts
+++ src/eventHub.ts
@@ -1,3 +1,18 @@
 /**
  * An eventhub to create event streams and let the event consumers subscribe to event queues
  */
+
+export default class EventHub {
+  subscribers: any;
+  constructor() {
+    this.subscribers = [];
+  }
+  subscribe(consumer: any) {
+    this.subscribers.push(consumer);
+  }
+  send(events: any) {
+    for (let subscriber of this.subscribers) {
+      subscriber.notify(events);
+    }
+  }
+}
