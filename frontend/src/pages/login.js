ccb02e722443558ded99a0151260a488e3f94896
hamidra <hamid.alipour@gmail.com>
2021-08-27 16:22:12 -0700

    style login page

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -8,63 +8,105 @@ import { loadExtension } from '../substrate-lib/extension';
 
 const Connecting = () => {
   return (
-    <Row className="p-md-5 justify-content-center">
-      <Col className="d-flex flex-column justify-content-center align-items-center text-center">
-        <div
-          style={{ height: '100px' }}
-          className="d-flex flex-column justify-content-around align-items-center">
-          <div>
-            <Spinner animation="border" role="status">
-              <span className="sr-only">Processing...</span>
-            </Spinner>
+    <>
+      <CardHeader
+        title="Login with your account"
+        cardText="Connecting to Polkadot Extension to load your account"
+      />
+      <Row className="p-md-5 justify-content-center">
+        <Col className="d-flex flex-column justify-content-center align-items-center text-center">
+          <div
+            style={{ height: '100px' }}
+            className="d-flex flex-column justify-content-around align-items-center">
+            <div>
+              <Spinner animation="border" role="status">
+                <span className="sr-only">Processing...</span>
+              </Spinner>
+            </div>
           </div>
-        </div>
-      </Col>
-    </Row>
+        </Col>
+      </Row>
+    </>
   );
 };
 
 const DownloadExtension = () => {
   const extensionLink = 'https://polkadot.js.org/extension/';
   return (
-    <Row className="p-md-5 justify-content-center">
-      <Col className="d-flex flex-column justify-content-center align-items-center text-center">
-        <a
-          style={{ height: '100px' }}
-          className="d-flex flex-column justify-content-around align-items-center"
-          href={extensionLink}
-          target="_blank"
-          rel="noopener noreferrer">
-          <DownloadSimple
-            className="flex-shrink-0 p-2 rounded icon"
-            size={68}
+    <>
+      <CardHeader
+        title="Login with your account"
+        cardText="No Polkadot extension was detected. Install polkadot extention and add your account to be able to login."
+      />
+      <Row className="p-md-5 justify-content-center">
+        <Col className="d-flex flex-column justify-content-center align-items-center text-center">
+          <a
+            style={{ height: '100px' }}
+            className="d-flex flex-column justify-content-around align-items-center"
+            href={extensionLink}
+            target="_blank"
+            rel="noopener noreferrer">
+            <DownloadSimple
+              className="flex-shrink-0 p-2 rounded icon"
+              size={68}
+            />
+            <div style={{ fontSize: '1rem' }} className="text-secondary">
+              Download Polkadot Extension
+            </div>
+          </a>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+const SelectAccount = ({ setAccountHandler, setAddressHandler, accounts }) => {
+  const [selectedAccount, setSelectedAccount] = useState(null);
+  const _setAccountHandler = () => {
+    setAccountHandler && setAccountHandler(selectedAccount);
+    setAddressHandler && setAddressHandler(selectedAccount?.address);
+  };
+  return (
+    <>
+      <CardHeader
+        title="Login with your account"
+        cardText="Login with your web3 extension account to manage your notifications"
+      />
+      <Row className="p-md-5 justify-content-center">
+        <Col className="d-flex flex-column justify-content-center align-items-center text-center">
+          <AccountSelector
+            accounts={accounts}
+            selectedAccount={selectedAccount}
+            setSelectedAccount={setSelectedAccount}
+            maxStrlength={15}
           />
-          <div style={{ fontSize: '1rem' }} className="text-secondary">
-            Download Polkadot Extension
-          </div>
-        </a>
-      </Col>
-    </Row>
+        </Col>
+      </Row>
+      <div className="d-flex flex-grow-1" />
+      <Row>
+        <Col className="pt-4 d-flex justify-content-center">
+          <button
+            className="btn btn-primary"
+            onClick={() => _setAccountHandler()}>
+            Connect
+          </button>
+        </Col>
+      </Row>
+    </>
   );
 };
 
 export default function ExtensionAccount({
   setAccountHandler,
   setAddressHandler,
-  title,
-  prevStepHandler,
 }) {
   const { dispatch, ...state } = useSubstrate();
   const { keyring, extensionState } = state;
-  const [selectedAccount, setSelectedAccount] = useState(null);
+
   const accounts = keyring.getPairs();
   useEffect(() => {
     loadExtension(state, dispatch);
   }, [dispatch, state]);
-  const _setAccountHandler = () => {
-    setAccountHandler && setAccountHandler(selectedAccount);
-    setAddressHandler && setAddressHandler(selectedAccount?.address);
-  };
 
   return (
     <>
@@ -75,35 +117,14 @@ export default function ExtensionAccount({
               style={{ width: 580, maxWidth: '100%', minHeight: 300 }}
               className="shadow">
               <Card.Body className="d-flex flex-column">
-                <CardHeader
-                  title="Login with extension account"
-                  cardText="Login with your extension account to manage your notifications"
-                />
                 {extensionState === 'NOT_AVAILABLE' ? (
                   <DownloadExtension />
                 ) : extensionState === 'READY' ? (
-                  <>
-                    <Row className="p-md-5 justify-content-center">
-                      <Col className="d-flex flex-column justify-content-center align-items-center text-center">
-                        <AccountSelector
-                          accounts={accounts}
-                          selectedAccount={selectedAccount}
-                          setSelectedAccount={setSelectedAccount}
-                          maxStrlength={15}
-                        />
-                      </Col>
-                    </Row>
-                    <div className="d-flex flex-grow-1" />
-                    <Row>
-                      <Col className="pt-4 d-flex justify-content-center">
-                        <button
-                          className="btn btn-primary"
-                          onClick={() => _setAccountHandler()}>
-                          Connect
-                        </button>
-                      </Col>
-                    </Row>
-                  </>
+                  <SelectAccount
+                    setAccountHandler={setAccountHandler}
+                    setAddressHandler={setAddressHandler}
+                    accounts={accounts}
+                  />
                 ) : (
                   <Connecting />
                 )}
