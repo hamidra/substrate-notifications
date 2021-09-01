5ee09fe67ceae16aad6c83e58fad91cfcb26f1a4
hamidra <hamid.alipour@gmail.com>
2021-08-31 20:48:20 -0700

    add web3 authorization (backend)

--- backend/src/authentication/index.js
+++ backend/src/authentication/index.js
@@ -1,6 +1,16 @@
-import {
-  getSubscriptions,
-  addAuthenticationToken,
-  revokeAuthenticationToken,
-} from '../controllers/subscriptionController';
-export const authenticate = (address, id_token) => {};
+import { verify_w3token } from './web3Auth';
+export const authenticate = (req) => {
+  let authorization = req.headers['authorization'];
+  let [authType, w3token, ...rest] = authorization?.split(' ') || [];
+  if (rest.length != 0 || authType.toLowerCase() != 'bearer' || !w3token) {
+    return { status: 403 };
+  }
+  let { error, header, payload } = verify_w3token(w3token) || {
+    error: 'was not able to verify the token',
+  };
+  if (error) {
+    console.log(error);
+    return { status: 401 };
+  }
+  return { status: 200, w3token, claimes: payload };
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import service from './service';
 import { Pallets } from './chain';
 import {
@@ -10,12 +11,17 @@ import {
   getNewAuthNonce,
   revokeAuthenticationToken,
 } from './controllers/subscriptionController';
+import { authenticate } from './authentication';
+import config from './config.json';
 
 const app = express();
 const port = process.env.HTTP_PORT || 8080; // default port to listen
 
+// set cookie parser
+app.use(cookieParser());
+
 // set cors
-app.use(cors());
+app.use(cors({ credentials: true, origin: config.ALLOW_ORIGIN }));
 
 // define a route handler for the default home page
 app.get('/management/unsubscribe/:address/:nonce', async (req, res) => {
@@ -64,7 +70,7 @@ app.get('/management/:address/:nonce', async (req, res) => {
 });
 
 // define a route handler for the default home page
-app.get('/login/:address/nonce', async (req, res) => {
+app.get('/authorize/:address/nonce', async (req, res) => {
   let { status, nonce } = await getNewAuthNonce(req.params.address);
   if (status < 400) {
     res.status(status).json({ nonce });
@@ -74,13 +80,17 @@ app.get('/login/:address/nonce', async (req, res) => {
 });
 
 // define a route handler for the default home page
-app.get('/logout/:address/:token', async (req, res) => {
-  let status = await revokeAuthenticationToken({
-    address: req.params.address,
-    auth_token: req.params.token,
-  });
-  if (status < 400) {
-    res.status(status).send(`${req.params.token} token was revoked`);
+app.get('/authorize', async (req, res) => {
+  let { status, w3token, claims } = authenticate(req) || {};
+  if (!status) {
+    console.log('something went wrong. not able to authenticate the request');
+    res
+      .status(500)
+      .send('something went wrong. not able to authenticate the request');
+  }
+  if (status === 200) {
+    res.cookie('w3token', w3token, { httponly: true });
+    res.status(status).send();
   } else {
     res.status(status).send();
   }
